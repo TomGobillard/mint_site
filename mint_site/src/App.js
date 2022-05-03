362b26c5c5470a163ecae077d4c9faa09ea63e95
Tom GOBILLARD <tgobillard@talent-bs.com>
2022-05-04 00:00:16 +0200

    build(dependencies): add dependecies

--- mint_site/src/App.js
+++ mint_site/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
+import { HashRouter,  Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <div className="container-fluid">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<App />} />
+            {/* <Route exact path='/' element={<Index />} /> */}
+            <Route exact path='/news' />
+            <Route exact path='/account'  />
+            <Route exact path='/news_form'  />
+            <Route exact path='/login'  />
+            <Route exact path='/register'/>
+          </Routes>
+        </div>
+      </HashRouter>
     </div>
   );
 }
